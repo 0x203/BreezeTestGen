d682c28221ce4a1b06c21a5d01330a44dc5ecf60
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-03-23 17:40:04 +0100

    create sceleton for informationHub

---
+++ src/main/scala/de/hpi/asg/breezetestgen/testgeneration/InformationHub.scala
@@ -0,0 +1,22 @@
+package de.hpi.asg.breezetestgen.testgeneration
+
+import de.hpi.asg.breezetestgen.constraintsolving.ConstraintCollection
+import de.hpi.asg.breezetestgen.domain.ComponentBehaviour.Reaction
+
+/** Central place for gathering information during a test/simulation run.
+  *
+  * Gathers [[de.hpi.asg.breezetestgen.testing.TestEvent]]s in a [[TestBuilder]],
+  * a [[de.hpi.asg.breezetestgen.constraintsolving.ConstraintCollection]] and (later) coverage statistics.
+  *
+  */
+class InformationHub(parentCollection: ConstraintCollection, testBuilder: TestBuilder) {
+  var cc: ConstraintCollection = parentCollection.fork()
+
+  def handleReaction(reaction: Reaction[_]): Unit = {
+    // cc = cc.addConstraints(reaction.cvs)
+    // call testbuilder according to testop
+    // extract coverage info from signals
+  }
+
+  def state = (cc, testBuilder)
+}
