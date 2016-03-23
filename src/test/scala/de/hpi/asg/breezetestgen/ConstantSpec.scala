6b00090b140322377303f4fdbc584f51dc7fb2c9
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-03-24 00:48:59 +0100

    add more tests for constant

--- src/test/scala/de/hpi/asg/breezetestgen/ConstantSpec.scala
+++ src/test/scala/de/hpi/asg/breezetestgen/ConstantSpec.scala
@@ -22,7 +22,16 @@ class ConstantSpec extends UnitTest {
     assert((c1 & c2) == Constant(5))
     assert((c1 | c2) == Constant(7))
     assert((c1 ^ c2) == Constant(2))
-
     //assert(c1.! == Constant(-5)) // TODO: implement this for unsigned constants
+  }
+
+  it should "not exceed it maximum domain value" in {
+    val c1 = Constant(1, 8)
+    val c257 = Constant(257, 8)
+
+    assert(c1.value == c257.value)
+
+    assert(Constant(8, 3).value < 8)
+    assert(Constant(8, 4, isSigned = true).value < 8)
   }
 }
