fe6dae80d508a63d991f5fc122bec6a901488f1a
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-03-23 16:47:05 +0100

    add classes for nodes of a test graph

---
+++ src/main/scala/de/hpi/asg/breezetestgen/testing/TestEvent.scala
@@ -0,0 +1,20 @@
+package de.hpi.asg.breezetestgen.testing
+
+import de.hpi.asg.breezetestgen.domain.{DataPort, SyncPort, Port}
+import scalax.collection.GraphEdge.DiEdge
+
+
+sealed trait TestEvent {
+  //TODO: remove this when next version of graph library is released
+  def ~>(other: TestEvent): DiEdge[TestEvent] = DiEdge(this, other)
+}
+
+class MergeEvent() extends TestEvent
+
+trait IOEvent[P <: Port] extends TestEvent {
+  val port: P
+}
+case class IOSyncEvent(port: SyncPort) extends IOEvent[SyncPort]
+case class IODataEvent(port: DataPort, value: Int) extends IOEvent[DataPort]
+
+//TODO: differentiate between req und ack events (or justify why not)
