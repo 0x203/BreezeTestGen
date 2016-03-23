4c40e174d4b0f9b09e5aaf709db3e9b05fabe6a1
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-03-23 18:49:50 +0100

    define TestOps

---
+++ src/main/scala/de/hpi/asg/breezetestgen/testgeneration/TestOp.scala
@@ -0,0 +1,11 @@
+package de.hpi.asg.breezetestgen.testgeneration
+
+import de.hpi.asg.breezetestgen.domain.{Data, DataPort, SyncPort}
+import de.hpi.asg.breezetestgen.testing.TestEvent
+
+
+sealed trait TestOp
+case class Split(after: TestEvent) extends TestOp
+case class Merge(after: Set[TestEvent]) extends TestOp
+case class AddSyncEvent(after: TestEvent, syncPort: SyncPort) extends TestOp
+case class AddDataEvent(after: TestEvent, dataPort: DataPort, d: Data) extends TestOp
