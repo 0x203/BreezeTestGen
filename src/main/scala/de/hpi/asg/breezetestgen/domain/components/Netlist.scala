f03651cca8ea7034e82eee803c331e29775ccfa0
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-03-24 18:27:55 +0100

    add sceletton for netlist component

---
+++ src/main/scala/de/hpi/asg/breezetestgen/domain/components/Netlist.scala
@@ -0,0 +1,38 @@
+package de.hpi.asg.breezetestgen.domain.components
+
+import de.hpi.asg.breezetestgen.domain.ComponentBehaviour.Reaction
+import de.hpi.asg.breezetestgen.domain._
+import de.hpi.asg.breezetestgen.testing.TestEvent
+
+/** represents a sub-netlist, i.e. a complete netlist wrapped into component which can be used in another netlist */
+class Netlist(val id: HandshakeComponent.Id, netlist: de.hpi.asg.breezetestgen.domain.Netlist)
+  extends HandshakeComponent {
+  import NetlistBehaviour._
+
+  type Behaviour = NetlistBehaviour
+  type C = ControlState
+  type D = PortStates
+
+  def behaviour(state: Option[ComponentState[C, D]]): Behaviour =
+    new NetlistBehaviour(state getOrElse NetlistBehaviour.freshState)
+
+  object NetlistBehaviour {
+    type ControlState = Null
+    type PortStates = Map[Port.Id, Port.State]
+
+    val freshState: ComponentState[ControlState, PortStates] =
+      ComponentState(null, netlist.ports.map{_.id -> Port.Idle}.toMap)
+  }
+
+  class NetlistBehaviour(initState: ComponentState[C, D]) {
+    def handleInternalSignal(port: Port, s: Signal, te: TestEvent): Reaction = {
+      var reaction = Reaction.empty
+      reaction
+    }
+
+    def handleExternalSignal(port: Port, s: Signal, te: TestEvent): Reaction = {
+      var reaction = Reaction.empty
+      reaction
+    }
+  }
+}
