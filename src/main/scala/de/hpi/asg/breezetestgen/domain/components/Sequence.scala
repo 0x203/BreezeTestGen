56cf02c95e4960b1b4b2d8bfae3ad01d19e170fc
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-03-24 15:00:23 +0100

    implment Sequence and SequenceSpec

---
+++ src/main/scala/de/hpi/asg/breezetestgen/domain/components/Sequence.scala
@@ -0,0 +1,56 @@
+package de.hpi.asg.breezetestgen.domain.components
+
+import de.hpi.asg.breezetestgen.domain._
+
+object Sequence {
+  val breezeName = "BrzSequence"
+}
+
+class Sequence(id: BrzComponent.Id,
+               activate: Channel.Spec[SyncChannel[_]],
+               outs: Seq[Channel.Spec[SyncChannel[_]]]) extends BrzComponent(id) {
+  type Behaviour = SequenceBehaviour
+  type C = SequenceBehaviour.ControlState
+  type D = Option[Int]
+
+  def behaviour(state: Option[ComponentState[C, D]]): Behaviour =
+    new SequenceBehaviour(state getOrElse SequenceBehaviour.freshState)
+
+
+  object SequenceBehaviour {
+    sealed trait ControlState
+    case object Idle extends ControlState
+    case object Called extends ControlState
+
+    val freshState: ComponentState[ControlState, D] = ComponentState(Idle, None)
+  }
+
+  class SequenceBehaviour(initState: ComponentState[C, D]) extends ComponentBehaviour[C, D](initState) {
+    import SequenceBehaviour._
+
+    info(s"SequenceBehaviour created in state: $initState")
+
+    when(Idle) {
+      case Req(`activate`, _) =>
+        info("Requested!")
+        request(outs.head)
+        goto(Called) using Some(0)
+    }
+
+    when(Called) {
+      case Ack(out, Some(i)) if out == outs(i) =>
+        val next_i = i + 1
+        if (next_i == outs.length) {
+          acknowledge(activate)
+          info("all finished. acknowledging...")
+          goto(Idle)
+        } else {
+          request(outs(next_i))
+          info(s"$next_i of ${outs.length} finished. requesting next...")
+          stay using Some(next_i)
+        }
+    }
+
+    initialize()
+  }
+}
---
+++ src/test/scala/de/hpi/asg/breezetestgen/SequenceSpec.scala
@@ -0,0 +1,62 @@
+package de.hpi.asg.breezetestgen
+
+import de.hpi.asg.breezetestgen.domain.ComponentBehaviour.Reaction
+import de.hpi.asg.breezetestgen.domain.components.Sequence
+import de.hpi.asg.breezetestgen.domain._
+import de.hpi.asg.breezetestgen.testing.{MergeEvent, TestEvent}
+
+
+class SequenceSpec extends baseclasses.UnitTest {
+  val activateId: Channel.Spec[SyncChannel[_]] = 1
+  val outIds: Seq[Channel.Spec[SyncChannel[_]]] = Seq(2, 3, 4)
+  val te: TestEvent = new MergeEvent()
+
+  val sequence = new Sequence(id = 0, activateId, outIds)
+
+  "A Sequence component" should "have a behaviour" in {
+    val sequenceBehaviour = sequence.behaviour(None)
+    assert(sequenceBehaviour.isInstanceOf[ComponentBehaviour[sequence.C, sequence.D]])
+  }
+
+  it should "respond to an request on activate with request first out" in {
+    val sequenceBehaviour = sequence.behaviour(None)
+    val activateRequest = Request(activateId)
+
+    assertResult(Reaction(Set(Request(outIds.head)), None, Set.empty)) {
+      sequenceBehaviour.handleSignal(activateRequest, te)
+    }
+  }
+
+  it should "throw an UnhandledException when getting another signal first" in {
+    val sequenceBehaviour = sequence.behaviour(None)
+    val inpAcknowledge = Acknowledge(outIds.last)
+
+    intercept[sequenceBehaviour.UnhandledException] {
+      sequenceBehaviour.handleSignal(inpAcknowledge, te)
+    }
+  }
+
+  it should "behave like BrzSequence would behave" in {
+    val sequenceBehaviour = sequence.behaviour(None)
+
+    val signals: Seq[Signal] = Request(activateId) +: outIds.map{id => Acknowledge(id)}
+    val reactions: Seq[Reaction] = outIds.map{id => Reaction(Set(Request(id)), None, Set.empty)} :+
+      Reaction(Set(Acknowledge(activateId)), None, Set.empty)
+
+    for ((s, r) <- signals.zip(reactions))
+      yield assertResult(r) {sequenceBehaviour.handleSignal(s, te)}
+  }
+
+  it should "be restorable from state" in {
+    val firstSequenceB = sequence.behaviour(None)
+    firstSequenceB.handleSignal(Request(activateId), te)
+
+    val state = firstSequenceB.state
+
+    val secondSequenceB = sequence.behaviour(Some(state))
+
+    assertResult(Reaction(Set(Request(outIds(1))), None, Set.empty)) {
+      secondSequenceB.handleSignal(Acknowledge(outIds.head), te)
+    }
+  }
+}
