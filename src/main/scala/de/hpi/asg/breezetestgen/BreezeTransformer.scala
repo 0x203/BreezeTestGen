b72036fa548c520e6af9a3ce1e6280d0910539b4
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-03-26 13:55:58 +0100

    breezeTransformer: transform channels correctly

--- src/main/scala/de/hpi/asg/breezetestgen/BreezeTransformer.scala
+++ src/main/scala/de/hpi/asg/breezetestgen/BreezeTransformer.scala
@@ -2,10 +2,12 @@ package de.hpi.asg.breezetestgen
 
 import de.hpi.asg.breezetestgen.domain.components.{BrzComponent, HandshakeComponent}
 import de.hpi.asg.breezetestgen.domain._
-import de.uni_potsdam.hpi.asg.common.breeze.model.{AbstractBreezeNetlist, BreezeProject, PortComponent}
+import de.uni_potsdam.hpi.asg.common.breeze.model._
 
-
+/** takes some netlist definition objects from asg.common and transform it to our domain objects */
 object BreezeTransformer {
+  //TODO: transform hierarchical netlists, too
+
   def parse(breezeFile: java.io.File): Option[Netlist] =
     Option(
       // TODO: verify hard coded path works in packaged version
@@ -14,16 +16,15 @@ object BreezeTransformer {
 
   def transformProject(breezeProject: BreezeProject): Option[Netlist] =
     breezeProject.getSortedNetlists.toArray().
-      lastOption.collectFirst{case a: AbstractBreezeNetlist=> a}.map(transfromNetlist)
+      lastOption.collectFirst{case a: AbstractBreezeNetlist=> a}.map(transformNetlist)
 
-  def transfromNetlist(baseNetlist: AbstractBreezeNetlist): Netlist = {
+  def transformNetlist(baseNetlist: AbstractBreezeNetlist): Netlist = {
 
     val id = Netlist.nextId
     val ports: Map[Port.Id, Port] = extractPorts(baseNetlist)
 
-    val channels: Map[Channel.Id, Channel[Channel.Endpoint]] = Map.empty
+    val channels: Map[Channel.Id, Channel[Channel.Endpoint]] = extractChannels(baseNetlist)
     val components: Map[HandshakeComponent.Id, BrzComponent] = Map.empty
-
 
     Netlist(id, ports, channels, components)
   }
@@ -59,5 +60,29 @@ object BreezeTransformer {
       // TODO: can we find out if data isSigned?
       DataPort(id, channelId, name, sense, direction, channel.getDatawidth)
     }
+  }
+
+  private def extractChannels(netlist: AbstractBreezeNetlist): Map[Channel.Id, Channel[Channel.Endpoint]] = {
+    import collection.JavaConversions.collectionAsScalaIterable
+    netlist.getChannelList.values.map(extractChannel).map{case c => c.id -> c}.toMap
+  }
+
+  private def extractChannel(raw: HSChannel): Channel[Channel.Endpoint] = {
+    val id: Channel.Id = raw.getId
+    val active: Channel.Endpoint = asEndpoint(raw.getActive)
+    val passive: Channel.Endpoint = asEndpoint(raw.getPassive)
+
+    if (raw.getDatawidth == 0)
+      SyncChannel(id, active, passive)
+    else raw.getDatatype match {
+      case HSChannel.DataType.pull => PullChannel(id, active, passive)
+      case HSChannel.DataType.push => PullChannel(id, active, passive)
+    }
+  }
+
+  private def asEndpoint(compInst: ComponentInst): Channel.Endpoint = compInst match {
+    case port: PortComponent => Channel.PortEndpoint(port.getId)
+    case hsComp: HSComponentInst => Channel.CompEndpoint(hsComp.getId)
+    case subNetlist: BreezeNetlistInst => throw new RuntimeException("Did not expect a subNetlist as target of channel")
   }
 }
