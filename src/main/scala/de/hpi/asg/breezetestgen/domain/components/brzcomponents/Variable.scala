7db2ee7021d9b74595bc06aebae933c43def3684
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-04-01 17:29:46 +0200

    implement Variable component

---
+++ src/main/scala/de/hpi/asg/breezetestgen/domain/components/brzcomponents/Variable.scala
@@ -0,0 +1,60 @@
+package de.hpi.asg.breezetestgen.domain.components.brzcomponents
+
+import de.hpi.asg.breezetestgen.domain.Data
+import de.hpi.asg.breezetestgen.domain.components.BrzComponent._
+import de.hpi.asg.breezetestgen.domain.components.brzcomponents.Variable.ReaderSpec
+import de.hpi.asg.breezetestgen.domain.components.{BrzComponent, BrzComponentBehaviour, HandshakeComponent}
+
+object Variable {
+  type ReaderSpec = (Int => Option[Range])
+}
+
+class Variable(id: HandshakeComponent.Id,
+               name: String,
+               readerSpec: ReaderSpec,
+               write: PushSpec,
+               reads: Seq[PullSpec]) extends BrzComponent(id) {
+  type Behaviour = VariableBehaviour
+  type C = VariableBehaviour.ControlState
+  type D = Option[Data]
+
+  def behaviour(state: Option[HandshakeComponent.State[C, D]]): Behaviour =
+    new VariableBehaviour(state getOrElse VariableBehaviour.freshState)
+
+
+  object VariableBehaviour {
+    sealed trait ControlState
+    case object Idle extends ControlState
+
+    val freshState: HandshakeComponent.State[ControlState, D] = HandshakeComponent.State(Idle, None)
+  }
+
+  class VariableBehaviour(initState: HandshakeComponent.State[C, D]) extends BrzComponentBehaviour[C, D](initState) {
+
+    import VariableBehaviour._
+
+    info(s"VariableBehaviour created in state:  $initState")
+
+    when(Idle) {
+      case DataReq(`write`, data, _) =>
+        info(s"$name is written: $data")
+        acknowledge(write)
+        stay using Option(data)
+
+      case Req(reader, Some(data)) if reads contains reader =>
+        val filteredData: Data = readerSpec(reads.indexOf(reader)) match {
+          case Some(range) if range.isEmpty => data
+          case Some(range) => data.selectBits(range)
+          case None =>
+            error("could not find specified range!")
+            data
+        }
+        info(s"$name is read: $filteredData")
+        dataAcknowledge(reader, filteredData)
+        stay
+    }
+
+    initialize()
+  }
+
+}
