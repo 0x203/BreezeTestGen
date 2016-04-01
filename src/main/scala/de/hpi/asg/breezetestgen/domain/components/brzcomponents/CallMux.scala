59490c9338b08294f8a20ed26866d13c1869cb05
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-04-01 17:02:12 +0200

    implement CallMux

---
+++ src/main/scala/de/hpi/asg/breezetestgen/domain/components/brzcomponents/CallMux.scala
@@ -0,0 +1,48 @@
+package de.hpi.asg.breezetestgen.domain.components.brzcomponents
+
+import de.hpi.asg.breezetestgen.domain.components.BrzComponent._
+import de.hpi.asg.breezetestgen.domain.components.{BrzComponent, BrzComponentBehaviour, HandshakeComponent}
+
+class CallMux(id: HandshakeComponent.Id,
+              inps: Set[PushSpec],
+              out: PushSpec) extends BrzComponent(id) {
+  type Behaviour = CallMuxBehaviour
+  type C = CallMuxBehaviour.ControlState
+  type D = Option[PushSpec]   // writer (inp) to acknowledge
+
+  def behaviour(state: Option[HandshakeComponent.State[C, D]]): Behaviour =
+    new CallMuxBehaviour(state getOrElse CallMuxBehaviour.freshState)
+
+
+  object CallMuxBehaviour {
+    sealed trait ControlState
+    case object Idle extends ControlState
+    case object Called extends ControlState
+
+    val freshState: HandshakeComponent.State[ControlState, D] = HandshakeComponent.State(Idle, None)
+  }
+
+  class CallMuxBehaviour(initState: HandshakeComponent.State[C, D]) extends BrzComponentBehaviour[C, D](initState) {
+
+    import CallMuxBehaviour._
+
+    info(s"CallMuxBehaviour created in state:  $initState")
+
+    when(Idle) {
+      case DataReq(inp, data, _) if inps contains inp =>
+        info("Requested!")
+        dataRequest(out, data)
+        goto(Called) using Some(inp)
+    }
+
+    when(Called) {
+      case Ack(`out`, Some(writer)) =>
+        info("Acknowledged")
+        acknowledge(writer)
+        goto(Idle) using None
+    }
+
+    initialize()
+  }
+
+}
