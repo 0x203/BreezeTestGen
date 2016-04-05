e23a3372a476676f9e63468ceb3eea1027886a57
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-04-05 20:00:20 +0200

    add working-dir handling for testfixtures

--- src/test/scala/de/hpi/asg/breezetestgen/fixtures/package.scala
+++ src/test/scala/de/hpi/asg/breezetestgen/fixtures/package.scala
@@ -1,9 +1,16 @@
 package de.hpi.asg.breezetestgen
 
 import de.hpi.asg.breezetestgen.domain.Netlist
+import de.uni_potsdam.hpi.asg.common.io.WorkingdirGenerator
 
 package object fixtures {
   def breezeFilePath = "/breezefiles/gcd.breeze"
   def breezeFile: java.io.File = new java.io.File(getClass.getResource(breezeFilePath).getPath)
-  def gcdNetlist(): Netlist = BreezeTransformer.parse(breezeFile).get
+  def gcdNetlist(): Netlist = {
+    WorkingdirGenerator.getInstance.create(null, null, "BrzTestGenTmp", null)
+    try
+      BreezeTransformer.parse(breezeFile).get
+    finally
+      WorkingdirGenerator.getInstance.delete()
+  }
 }
