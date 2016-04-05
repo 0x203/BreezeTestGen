3b7808d01f14b396b135faf45d2ad2f5697688a1
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-04-05 19:45:31 +0200

    remove ad-hoc testing code from Main

--- src/main/scala/de/hpi/asg/breezetestgen/Main.scala
+++ src/main/scala/de/hpi/asg/breezetestgen/Main.scala
@@ -1,15 +1,7 @@
 package de.hpi.asg.breezetestgen
 
 import java.io.File
-
-import akka.actor.{ActorSystem, Props}
-import de.hpi.asg.breezetestgen.actors.Simulator
 import de.uni_potsdam.hpi.asg.common.io.WorkingdirGenerator
-
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
-import scalax.collection.GraphEdge.DiEdge
-import scalax.collection.immutable.Graph
 
 object Main {
   val logger = Logging.getLogger(this)
@@ -54,55 +46,8 @@ object Main {
     mainNetlist match {
       case Some(netlist) =>
         logger.info(netlist)
-        val test = sampleGCDTest(netlist)
-        simulate(netlist, test)
       case None => logger.error("Could not parse Netlist")
     }
-  }
-
-  private def sampleGCDTest(netlist: domain.Netlist): testing.Test = {
-    import de.hpi.asg.breezetestgen.domain._
-    import de.hpi.asg.breezetestgen.testing._
-    def findPort(name: String): Port = netlist.ports.values.find(_.name == name).get
-    val activateId = findPort("activate").channelId
-    val ainId = findPort("ain").channelId
-    val binId = findPort("bin").channelId
-    val oId = findPort("o").channelId
-
-    val activateReq = IOEvent(Request(activateId))
-    val activateAck = IOEvent(Acknowledge(activateId))
-
-    val ainReq = IOEvent(Request(ainId))
-    val ainAck = IOEvent(DataAcknowledge(ainId, Constant(12)))
-
-    val binReq = IOEvent(Request(binId))
-    val binAck = IOEvent(DataAcknowledge(binId, Constant(8)))
-
-    val oReq = IOEvent(DataRequest(oId, Constant(4)))
-    val oAck = IOEvent(Acknowledge(oId))
-
-    val merge = new MergeEvent
-
-    Graph[TestEvent, DiEdge](
-      activateReq ~> ainReq,
-      activateReq ~> binReq,
-      ainReq ~> ainAck,
-      binReq ~> binAck,
-      ainAck ~> merge,
-      binAck ~> merge,
-      merge ~> oReq,
-      oReq ~> oAck,
-      oAck ~> activateAck
-    )
-  }
-
-  private def simulate(netlist: domain.Netlist, test: testing.Test) = {
-    val system = ActorSystem("Simulator")
-    logger.info("Start simulation...")
-    val simulator = system.actorOf(Props(classOf[Simulator], netlist, test))
-    simulator ! Simulator.RunTest
-    Await.result(system.whenTerminated, Duration.Inf)
-    logger.info("Simulation finished!")
   }
 }
 
