0644b65045f4abd45e16bc554837ab75113772f7
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-04-07 11:24:24 +0200

    prepare ComponentActor to take care of decision required signals

--- src/main/scala/de/hpi/asg/breezetestgen/actors/ComponentActor.scala
+++ src/main/scala/de/hpi/asg/breezetestgen/actors/ComponentActor.scala
@@ -3,6 +3,7 @@ package de.hpi.asg.breezetestgen.actors
 import akka.actor.ActorRef
 import de.hpi.asg.breezetestgen.actors.HandshakeActor.{GetState, MyState, Signal}
 import de.hpi.asg.breezetestgen.domain
+import de.hpi.asg.breezetestgen.domain.components.BrzComponentBehaviour.{DecisionRequired, NormalFlowReaction, Reaction}
 import de.hpi.asg.breezetestgen.domain.components.{BrzComponentBehaviour, HandshakeComponent}
 import de.hpi.asg.breezetestgen.domain.{Netlist, SignalFromActive, SignalFromPassive}
 import de.hpi.asg.breezetestgen.testing.TestEvent
@@ -12,30 +13,36 @@ class ComponentActor(netlistId: Netlist.Id,
                      component: BrzComponentBehaviour[_, _],
                      infoHub: Option[ActorRef]) extends HandshakeActor {
 
+  receiue{
+    case GetState => sender() ! MyState(netlistId, componentId, component.state)
+  }
+
   override protected def handleSignal(nlId: Netlist.Id, ds: domain.Signal, testEvent: TestEvent) = {
     require(netlistId == nlId)
 
-    val reaction = component.handleSignal(ds, testEvent)
-
-    //TODO: handle decision making!
-
-    // handle TestOp
-    val newTestEvent = reaction.testOp match {
-      case None =>  testEvent
-      case Some(op) =>
-        //TODO: perform testOp and get new TestEvent
-        testEvent
+    component.handleSignal(ds, testEvent) match {
+      case nf: NormalFlowReaction => handleNormalFlow(nlId, testEvent, nf)
+      case dr: DecisionRequired => handleDecisionRequired(nlId, testEvent, dr)
     }
-
-    // send out constraints
-    infoHub.foreach(_ ! reaction.constraintVariables)
-
-    // send out new signals
-    sendOutSignals(netlistId, newTestEvent, reaction.signals)
   }
 
-  receiue{
-    case GetState => sender() ! MyState(netlistId, componentId, component.state)
+  private def handleDecisionRequired(nlId: Netlist.Id, testEvent: TestEvent, dr: DecisionRequired) = {
+    infoHub match {
+      case Some(hub) =>
+        //TODO: let others decide and set state accordingly
+        //val decisionF = hub ? dr
+      case None =>
+          error("No InformationHub given, but DecisionRequired!")
+          context.stop(self)
+    }
+  }
+
+  private def handleNormalFlow(nlId: Netlist.Id, testEvent: TestEvent, nf: NormalFlowReaction) = {
+
+    //TODO implemnt something like this:
+    //val newTestEventF = infoHub.map(_ ? nf).asInstanceOf[Future[TestEvent] orElse Future[TestEvent] {testEvent}
+    //newTestEventF.then(sendOutSignals(nlId, _, nf.signals))
+
   }
 
   private def sendOutSignals(netlist: Netlist.Id, testEvent: TestEvent, ds: Set[domain.Signal]) = {
