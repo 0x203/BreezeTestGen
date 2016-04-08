f2dfca0201feadb9d95090dac357c243c3ceb656
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-04-08 20:35:23 +0200

    increase timeout for akka specs

--- src/test/scala/de/hpi/asg/breezetestgen/baseclasses/AkkaIntegrationSpec.scala
+++ src/test/scala/de/hpi/asg/breezetestgen/baseclasses/AkkaIntegrationSpec.scala
@@ -2,12 +2,17 @@ package de.hpi.asg.breezetestgen.baseclasses
 
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit}
+import akka.util.Timeout
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike}
+
+import concurrent.duration._
 
 
 abstract class AkkaIntegrationSpec(_system: ActorSystem)
   extends TestKit(_system) with FlatSpecLike with ImplicitSender with BeforeAndAfterAll {
   def this(name: String) = this(ActorSystem(name))
+
+  implicit val timeout = Timeout(7 seconds)
 
   override def afterAll {
     TestKit.shutdownActorSystem(system)
