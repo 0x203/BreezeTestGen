ff7379ef17461731f08b273595db4efb3fd32923
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-06-07 23:36:01 +0200

    WIP transform whole test graph to add ids before serialization

--- src/main/scala/de/hpi/asg/breezetestgen/testing/JsonFromTo.scala
+++ src/main/scala/de/hpi/asg/breezetestgen/testing/JsonFromTo.scala
@@ -4,10 +4,34 @@ import de.hpi.asg.breezetestgen.domain.{Acknowledge, DataAcknowledge, DataReques
 import net.liftweb.json.CustomSerializer
 import net.liftweb.json.JsonAST.{JField, JInt, JObject}
 
+import scalax.collection.GraphEdge.DiEdge
+import scalax.collection.GraphPredef
 import scalax.collection.io.json._
 import scalax.collection.io.json.descriptor.predefined.Di
 
+/** Helps transforming an instance of a test graph to a json representation and the other way round
+  */
 object JsonFromTo {
+
+  /** adds an ID to an IOEvent in order to be adresseable by edges */
+  private case class TestEventWithID(id: Int, orig: TestEvent)
+
+  /** transform a testgrpah without IDs to one with IDs */
+  private def addIDs(orig: Test): scalax.collection.Graph[TestEventWithID, DiEdge] = {
+    val ids = (1 to Int.MaxValue).iterator
+
+    val nodeMap = orig.nodes
+      .map{ x => x -> TestEventWithID(ids.next(), x)}
+      .toMap
+
+    // replace placeholders in all edge definitions
+    val edges: Set[GraphPredef.Param[TestEventWithID, DiEdge]] = Set() ++ orig.edges.map{
+      e => DiEdge(nodeMap(e.source), nodeMap(e.target)).asInstanceOf[GraphPredef.Param[TestEventWithID, DiEdge]]
+    }
+
+    // create a new, immutable graph from the edges and nodes
+    scalax.collection.Graph[TestEventWithID, DiEdge]() ++ (edges ++ nodeMap.values.map(GraphPredef.OuterNode[TestEventWithID]))
+  }
 
 
   private def idGeneratingDescriptor(): Descriptor[TestEvent] = {
@@ -58,7 +82,21 @@ object JsonFromTo {
 
 
   def toJson(test: Test): String = {
-    test.toJson(idGeneratingDescriptor())
+    val testWithIds = addIDs(test)
+
+    val nodeDescriptor = new NodeDescriptor[TestEventWithID](typeId = "TestEvent"){
+      def id(node: Any) = node match {
+        case TestEventWithID(id, _) => id.toString
+      }
+    }
+    val desc = new Descriptor[TestEventWithID](
+      defaultNodeDescriptor = nodeDescriptor,
+      defaultEdgeDescriptor = Di.descriptor[TestEventWithID](),
+      namedNodeDescriptors = Seq(nodeDescriptor),
+      namedEdgeDescriptors = Seq(Di.descriptor[TestEventWithID]())
+    )
+
+    testWithIds.toJson(desc)
   }
 
   def fromJson(test: String): Option[Test] = {
