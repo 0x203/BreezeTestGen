b15cc69ec71e5e7afc03386e8ff720e8efe9c9b5
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-06-07 20:37:45 +0200

    create stub object for Json import export

---
+++ src/main/scala/de/hpi/asg/breezetestgen/testing/JsonFromTo.scala
@@ -0,0 +1,11 @@
+package de.hpi.asg.breezetestgen.testing
+
+object JsonFromTo {
+  def toJson(test: Test): String = {
+    ""
+  }
+
+  def fromJson(test: String): Option[Test] = {
+    None
+  }
+}
