284df180944793640e7f6ab25d825877d7f022a4
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-06-08 20:23:55 +0200

    docs for test instantiator

--- src/main/scala/de/hpi/asg/breezetestgen/testgeneration/constraintsolving/TestInstantiator.scala
+++ src/main/scala/de/hpi/asg/breezetestgen/testgeneration/constraintsolving/TestInstantiator.scala
@@ -7,6 +7,7 @@ import de.hpi.asg.breezetestgen.testing.Test
   * create one or more tests out of it
    */
 object TestInstantiator {
+  /** return the first working test if any exists */
   def first(cc: ConstraintCollection, tb: TestBuilder): Option[Test] = {
     val solver = new ChocoSolver(cc)
     if(solver.isFeasible) {
@@ -14,6 +15,7 @@ object TestInstantiator {
     } else None
   }
 
+  /** return a randomly selected test, if any exists */
   def random(cc: ConstraintCollection, tb: TestBuilder): Option[Test] = {
     val solver = new ChocoSolver(cc)
     if(solver.isFeasible) {
@@ -25,6 +27,14 @@ object TestInstantiator {
 
   private val randomGenerator = new util.Random()
 
+  /** select a random element from a iterator of unknown length
+    * works by replacing the ith element with probability 1/i with the currently chosen one
+    *
+    * Examples:
+    *   length 1: 1/1 for first
+    *   length 2: 1/2 for first, 1/2 for second
+    *   and so on
+    */
   private def choose[A](it: Iterator[A], r: util.Random): A =
   it.zip(Iterator.iterate(1)(_ + 1)).reduceLeft((x, y) =>
     if (r.nextInt(y._2) == 0) y else x
