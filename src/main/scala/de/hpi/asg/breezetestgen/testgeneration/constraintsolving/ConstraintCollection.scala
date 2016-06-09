de18826eb90fd23037a157486df8fa30fd16cf45
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-06-09 14:37:30 +0200

    correct ConstraintCollection insert just CVs that are not present yet

--- src/main/scala/de/hpi/asg/breezetestgen/testgeneration/constraintsolving/ConstraintCollection.scala
+++ src/main/scala/de/hpi/asg/breezetestgen/testgeneration/constraintsolving/ConstraintCollection.scala
@@ -4,13 +4,23 @@ package de.hpi.asg.breezetestgen.testgeneration.constraintsolving
 case class ConstraintCollection(parent: Option[ConstraintCollection] = None,
                                 variables: Set[Variable] = Set.empty,
                                 constraints: Set[Constraint] = Set.empty) {
-  def addVariable(v: Variable): ConstraintCollection = this.copy(variables = variables + v)
-  def addVariables(vs: Traversable[Variable]): ConstraintCollection = this.copy(variables = variables ++ vs)
-  def addConstraint(c: Constraint): ConstraintCollection = this.copy(constraints= constraints + c)
-  def addConstraints(cs: Traversable[Constraint]): ConstraintCollection = this.copy(constraints= constraints ++ cs)
+  def addVariable(v: Variable): ConstraintCollection =
+    if (allVariables contains v) {
+      this
+    } else {
+      this.copy(variables = variables + v)
+    }
+
+  def addConstraint(c: Constraint): ConstraintCollection =
+    if (allConstraints contains c) {
+      this
+    } else {
+      this.copy(constraints= constraints + c)
+    }
+
   def add(cvs: Traversable[ConstraintVariable]): ConstraintCollection = this.copy(
-      constraints = constraints ++ cvs.collect{case c: ConstraintCV => c.c},
-      variables = variables ++ cvs.collect{case v: VariableCV => v.v}
+      constraints = cvs.collect{case c: ConstraintCV => c.c}.toSet -- allConstraints ++ constraints ,
+      variables = cvs.collect{case v: VariableCV => v.v}.toSet -- allVariables ++ variables
   )
 
   def allVariables: Set[Variable] = parent.map(_.variables).getOrElse(Set.empty) ++ variables
