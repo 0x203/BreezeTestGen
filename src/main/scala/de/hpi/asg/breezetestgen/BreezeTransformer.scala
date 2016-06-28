d13a8ed34a6716fab66337869859f69aad7eed7c
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-06-29 00:14:30 +0200

    transformer rule for caseFetch

--- src/main/scala/de/hpi/asg/breezetestgen/BreezeTransformer.scala
+++ src/main/scala/de/hpi/asg/breezetestgen/BreezeTransformer.scala
@@ -114,6 +114,8 @@ object ComponentExtractors {
     raw.getBrzStr match {
       case "BrzCallMux" => new CallMux(id, channelSet(0), channel(1))
       case "BrzCase" => new Case(id, CaseSpecParser.fromString(stringParam(2), intParam(0)), channel(0), channelSeq(1))
+      case "BrzCaseFetch" =>
+        new CaseFetch(id, CaseSpecParser.fromString(stringParam(3), intParam(1)), channel(0), channel(1), channelSeq(2))
       case "BrzConcur" => new Concur(id, channel(0), channelSet(1))
       case "BrzConstant" => new Constant(id, channel(0), Constant(intParam(1), intParam(0)))
       case "BrzBinaryFunc" => new BinaryFunc(id, stringParam(3), channel(0), channel(1), channel(2))
