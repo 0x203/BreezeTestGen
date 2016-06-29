567466c8e3edcb65b3d20b76bf9307699665e881
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-06-29 20:45:08 +0200

    implement first version of adapt

---
+++ src/main/scala/de/hpi/asg/breezetestgen/domain/components/brzcomponents/Adapt.scala
@@ -0,0 +1,65 @@
+package de.hpi.asg.breezetestgen.domain.components.brzcomponents
+
+import de.hpi.asg.breezetestgen.domain.components.{BrzComponent, BrzComponentBehaviour, HandshakeComponent}
+import BrzComponent._
+import de.hpi.asg.breezetestgen.domain.Data
+
+class Adapt(id: HandshakeComponent.Id,
+            outWidth: Int,
+            inWidth: Int,
+            outSigned: Boolean,
+            inSigned: Boolean,
+            inp: PullSpec,
+            out: PullSpec) extends BrzComponent(id) {
+  type Behaviour = AdaptBehaviour
+  type C = AdaptBehaviour.ControlState
+  type D = Null
+
+  def behaviour(state: Option[HandshakeComponent.State[C, D]]): Behaviour =
+    new AdaptBehaviour(state getOrElse AdaptBehaviour.freshState)
+
+  def adapt(in: Data): Data = {
+    if(outSigned == inSigned && outSigned >= inSigned)
+      in
+    else {
+      //TODO implement me
+      throw new NotImplementedError("Don't have a way to declare adaption of values")
+    }
+  }
+
+
+  object AdaptBehaviour {
+    sealed trait ControlState
+    case object Idle extends ControlState
+    case object Fetching extends ControlState
+
+    val freshState: HandshakeComponent.State[C, D] = HandshakeComponent.State(Idle, null)
+  }
+
+  class AdaptBehaviour(initState: HandshakeComponent.State[C, D]) extends BrzComponentBehaviour[C, D](initState) {
+    import AdaptBehaviour._
+
+    info(s"AdaptBehaviour created in state: $initState")
+
+    when(Idle) {
+      case Req(`out`, _) =>
+        info(s"Requested!")
+        request(inp)
+        goto(Fetching)
+    }
+
+    when(Fetching) {
+      case DataAck(`inp`, inData, _) =>
+        info(s"Got input data: $inData")
+        if ((inData.bitCount != inWidth) | (inData.isSigned != inSigned))
+          error(s"Expecting bitcount $inWidth and signed $inSigned; got ${inData.bitCount} and ${inData.isSigned}")
+
+        val outData = adapt(inData)
+        dataAcknowledge(out, outData)
+        goto(Idle)
+    }
+
+    initialize()
+  }
+
+}
